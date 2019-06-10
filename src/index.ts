733955823bd7ecfcca15a217b962e6ba9ffc6ee0
Pavol Hejný <me@pavolhejny.com>
2019-06-10 18:30:45 +0200

    Repairs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { ConfigChecker } from './classes/ConfigChecker';
+import { IConfigSource } from './interfaces/IConfigSource';
+import { IConfigValueProfile } from './interfaces/IConfigValueProfile';
 import * as ObjectUtils from './utils/object';
 
-export { ConfigChecker, ObjectUtils };
+export { ConfigChecker, IConfigSource, IConfigValueProfile, ObjectUtils };
