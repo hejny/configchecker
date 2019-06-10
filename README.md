9b0c1e3f49a5d5066a00f84fadd5f0960df13eab
Pavol Hejný <me@pavolhejny.com>
2019-06-10 19:01:21 +0200

    Full module

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Awesome checker of config - fully support typescript, fluent API,...
 
-__WARNING__ this project works only with typescript not pure javascript.
-
 # Usage
 
 __TODO:__
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,25 @@
-//TODO:
+import { ConfigChecker } from '../src/classes/ConfigChecker';
 
-export {};
+test('ConfigChecker', () => {
+    const c = ConfigChecker.from({
+        string: 'foo',
+        stringEmpty: '',
+        numeric: '1',
+        numericInvalid: 'foo1foo', // TODO: Also with 1foo
+        boolean: 'True',
+        booleanInvalid: 'foo',
+        // TODO: Others
+    });
+
+    expect(() => c.get('undefined').value).not.toThrowError();
+    expect(() => c.get('undefined').required().value).toThrowError();
+
+    expect(() => c.get('string').value).not.toThrowError();
+    expect(() => c.get('stringEmpty').value).not.toThrowError();
+
+    expect(() => c.get('numeric').number().value).not.toThrowError();
+    expect(() => c.get('numericInvalid').number().value).toThrowError();
+
+    expect(() => c.get('boolean').boolean().value).not.toThrowError();
+    expect(() => c.get('booleanInvalid').boolean().value).toThrowError();
+});
